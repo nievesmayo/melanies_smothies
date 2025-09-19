2256f30ee6ce56a625a22d6de66f9a1d72492ea3
nievesmayo <nieves.mayo@accenture.com>
2025-09-19 18:05:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,8 +50,8 @@ if ingredients_list:
         #st.write('The search value for ', fruit,' is ', search_on, '.')
       
         st.subheader(fruit + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        sf_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     
 
     #st.write(ingredients_string)
