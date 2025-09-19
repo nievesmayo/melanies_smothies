e83f432b597041e1eb75a4b478834074510a594a
nievesmayo <nieves.mayo@accenture.com>
2025-09-19 17:06:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,7 @@
 
 
 import streamlit as st
-from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col
-
-# Crear sesión Snowflake usando secretos guardados en Streamlit Cloud
-session = Session.builder.configs(st.secrets).create()
 
 
 # Write directly to the app
@@ -20,7 +16,10 @@ st.write(
 name_on_order = st.text_input("Name of Smoothie")
 st.write("The name on your Smoothie will be", name_on_order)
 
-session = get_active_session()
+cnx=st.connection("snowflake")
+session=cnx.session()
+
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
